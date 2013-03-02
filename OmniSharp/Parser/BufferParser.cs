OmniSharp/omnisharp-vim

commit d8dfb228ade0a65cbaf9820c3eb6ea986e04b56e

find usages single quote fix

OmniSharp/Parser/BufferParser.cs
@@ -39,7 +39,10 @@ namespace OmniSharp.Parser
                 IUnresolvedFile oldFile = pctx.GetFile(filename);
                 pctx = pctx.AddOrUpdateFiles(oldFile, parsedFile);
             }
-            
+
+            var editedFile = _solution.GetFile(filename);
+            editedFile.Update(editorText);
+
             project.ProjectContent = pctx;
             ICompilation cmp = pctx.CreateCompilation();
 

OmniSharp/FindUsages/FindUsagesHandler.cs
@@ -28,8 +28,7 @@ namespace OmniSharp.FindUsages
 
         public FindUsagesResponse FindUsages(FindUsagesRequest request)
         {
-            var editedFile = _solution.GetFile(request.FileName);
-            editedFile.Update(request.Buffer);
+            
             var res = _parser.ParsedContent(request.Buffer, request.FileName);
             var loc = new TextLocation(request.Line, request.Column);
             var result = new ConcurrentBag<AstNode>();
@@ -81,7 +80,7 @@ namespace OmniSharp.FindUsages
             var usages = result.Select(node => new Usage
             {
                 FileName = node.GetRegion().FileName,
-                Text = node.Preview(_solution.GetFile(node.GetRegion().FileName)),
+                Text = node.Preview(_solution.GetFile(node.GetRegion().FileName)).Replace("'", "''"),
                 Line = node.StartLocation.Line,
                 Column = node.StartLocation.Column,
             });

commit 8303f355f87a23f7aa92a78e7b089a6097146134

Fixed regression introduced in d8dfb22: completion in project-less files casues a NullPointerException.

OmniSharp/Parser/BufferParser.cs
@@ -41,7 +41,10 @@ namespace OmniSharp.Parser
             }
 
             var editedFile = _solution.GetFile(filename);
-            editedFile.Update(editorText);
+            //If GetFile couldn't find a file, it will return null
+            //this will happen when an project-less file is loaded
+            if (editedFile != null)
+                editedFile.Update(editorText);
 
             project.ProjectContent = pctx;
             ICompilation cmp = pctx.CreateCompilation();