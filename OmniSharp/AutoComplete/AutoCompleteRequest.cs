e789ec610f84d0e7886907807b6511cdac0b7e8f
waedt <wayedt@gmail.com>
2013-03-03 23:29:04 -0600

    Fixed mess-up with previous merge.

--- OmniSharp/AutoComplete/AutoCompleteRequest.cs
+++ OmniSharp/AutoComplete/AutoCompleteRequest.cs
@@ -4,8 +4,6 @@ namespace OmniSharp.AutoComplete
 {
     public class AutoCompleteRequest : Request
     {
-        public int Line { get; set; }
-        public int Column { get; set; }
         public string WordToComplete { get; set; }
     }
 }
