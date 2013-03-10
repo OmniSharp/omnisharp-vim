a804a2385b40ef47aa3b69b7a89397f80e051735
waedt <wayedt@gmail.com>
2013-03-09 23:05:39 -0600

    Rough in TypeLookup classes.

---
+++ OmniSharp/TypeLookup/TypeLookupHandler.cs
@@ -0,0 +1,32 @@
+﻿using ICSharpCode.NRefactory;
+using ICSharpCode.NRefactory.CSharp.Resolver;
+using ICSharpCode.NRefactory.Semantics;
+using OmniSharp.Parser;
+
+namespace OmniSharp.TypeLookup
+{
+    public class TypeLookupHandler
+    {
+        private readonly BufferParser _bufferParser;
+
+        public TypeLookupHandler(BufferParser bufferParser)
+        {
+            _bufferParser = bufferParser;
+        }
+
+        public TypeLookupResponse GetTypeLookupResponse(TypeLookupRequest request)
+        {
+            var res = _bufferParser.ParsedContent(request.Buffer, request.FileName);
+            var loc = new TextLocation(request.Line, request.Column);
+            ResolveResult resolveResult = ResolveAtLocation.Resolve(res.Compilation, res.UnresolvedFile, res.SyntaxTree, loc);
+            var response = new TypeLookupResponse();
+            if (resolveResult != null)
+            {
+                response.Type = resolveResult.ToString();
+            }
+
+            return response;
+        }
+
+    }
+}
---
+++ OmniSharp/TypeLookup/TypeLookupModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+
+namespace OmniSharp.TypeLookup
+{
+    public class TypeLookupModule : NancyModule
+    {
+        public TypeLookupModule(TypeLookupHandler typeLookupHandler)
+        {
+            Post["/typelookup"] = x =>
+                {
+                    var req = this.Bind<TypeLookupRequest>();
+                    var res = typeLookupHandler.GetTypeLookupResponse(req);
+                    return Response.AsJson(res);
+                };
+        }
+    }
+}
