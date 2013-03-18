ae64dd3cb03c6d540a255807ed0674cb32d6a36e
nosami <jasonimison@gmail.com>
2013-03-18 19:11:04 +0000

    added missing files

---
+++ OmniSharp.Tests/Rename/SimpleRenameTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using OmniSharp.FindUsages;
+using OmniSharp.Parser;
+using OmniSharp.Rename;
+using Should;
+
+namespace OmniSharp.Tests.Rename
+{
+    [TestFixture]
+    public class SimpleRenameTests
+    {
+        private string Rename(string buffer, string renameTo)
+        {
+            var location = TestHelpers.GetLineAndColumnFromDollar(buffer);
+            buffer = buffer.Replace("$", "");
+            var solution = new FakeSolution();
+            var project = new FakeProject();
+            project.AddFile(buffer);
+            solution.Projects.Add("dummyproject", project);
+            var bufferParser = new BufferParser(solution);
+            var renameHandler = new RenameHandler(bufferParser, new FindUsagesHandler(bufferParser, solution));
+            var request = new RenameRequest
+                {
+                    Buffer = buffer,
+                    Column = location.Column - 1,
+                    Line = location.Line,
+                    RenameTo = renameTo,
+                    FileName = "myfile"
+                };
+
+            var response = renameHandler.Rename(request);
+            return response.Changes.First().Buffer;
+        }
+
+        [Test]
+        public void Should_rename_variable()
+        {
+            Rename(
+@"public class MyClass
+{
+    public MyClass()
+    {
+        var s$ = String.Empty;
+    }
+}", "str").ShouldEqual(
+@"public class MyClass
+{
+    public MyClass()
+    {
+        var str = String.Empty;
+    }
+}" 
+ );
+        }
+
+        [Test]
+        public void Should_rename_variable_and_usage()
+        {
+            Rename(
+@"public class MyClass
+{
+    public MyClass()
+    {
+        var s$ = ""s"";
+        s = s + s;
+    }
+}", "str").ShouldEqual(
+@"public class MyClass
+{
+    public MyClass()
+    {
+        var str = ""s"";
+        str = str + str;
+    }
+}"
+ );
+        }
+    }
+
+
+}
---
+++ OmniSharp.Tests/TestHelpers.cs
@@ -0,0 +1,25 @@
+﻿using ICSharpCode.NRefactory;
+
+namespace OmniSharp.Tests
+{
+    public static class TestHelpers
+    {
+        public static TextLocation GetLineAndColumnFromDollar(string text)
+        {
+            return GetLineAndColumnFromIndex(text, text.IndexOf("$"));
+        }
+
+        public static TextLocation GetLineAndColumnFromIndex(string text, int index)
+        {
+            int lineCount = 1, lastLineEnd = -1;
+            for (int i = 0; i < index; i++)
+                if (text[i] == '\n')
+                {
+                    lineCount++;
+                    lastLineEnd = i;
+                }
+
+            return new TextLocation(lineCount, index - lastLineEnd);
+        }
+    }
+}
