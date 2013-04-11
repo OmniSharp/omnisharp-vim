d531138c55c203368cb15ec10ab8a436635aa00a
nosami <jasonimison@gmail.com>
2013-04-11 21:38:06 +0100

    merged build

---
+++ OmniSharp/Common/QuickFix.cs
@@ -0,0 +1,10 @@
+﻿namespace OmniSharp.Common
+{
+    public class QuickFix
+    {
+        public string FileName { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ OmniSharp/Common/Request.cs
@@ -0,0 +1,17 @@
+﻿using OmniSharp.Solution;
+
+namespace OmniSharp.Common
+{
+    public class Request
+    {
+        private string _fileName;
+        public int Line { get; set; }
+        public int Column { get; set; }
+        public string Buffer { get; set; }
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value.FixPath(); }
+        }
+    }
+}
