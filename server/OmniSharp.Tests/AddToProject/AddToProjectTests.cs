8df3b85cd5fbb5361ecfa476e3a401c7288e0b7e
Gareth Williams <gw@gdoubleu.co.uk>
2013-05-10 09:47:51 +0100

    Adding tests for AddToProject functionality

---
+++ server/OmniSharp.Tests/AddToProject/AddToProjectTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Linq;
+using NUnit.Framework;
+using OmniSharp.AddToProject;
+
+namespace OmniSharp.Tests.AddToProject
+{
+    [TestFixture]
+    public class AddToProjectTests
+    {
+         [Test]
+         public void ShouldNotAddFileToProjectWhenAlreadyExists()
+         {
+             var project = new FakeProject(fileName: @"c:\test\code\fake.csproj");
+             project.AddFile("some content", @"c:\test\code\test.cs");
+
+             project.XmlRepresentation = XDocument.Parse(@"<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003""><ItemGroup><Compile Include=""Hello.cs""/><Compile Include=""Test.cs""/></ItemGroup></Project>");
+             var expectedXml = project.XmlRepresentation;
+
+             var solution = new FakeSolution(@"c:\test\fake.sln");
+             solution.Projects.Add(project);
+
+             var request = new AddToProjectRequest
+                 {
+                     FileName = @"c:\test\code\test.cs"
+                 };
+
+             var handler = new AddToProjectHandler(solution);
+             handler.AddToProject(request);
+
+             Assert.That(project.AsXml(), Is.EqualTo(expectedXml));
+         }
+
+        [Test]
+        public void ShouldAddNewFileToProject()
+        {
+            var project = new FakeProject(fileName: @"c:\test\code\fake.csproj");
+            project.AddFile("some content", @"c:\test\code\Test.cs");
+
+            project.XmlRepresentation = XDocument.Parse(@"<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003""><ItemGroup><Compile Include=""Hello.cs""/></ItemGroup></Project>");
+
+            var expectedXml = XDocument.Parse(@"<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003""><ItemGroup><Compile Include=""Hello.cs""/><Compile Include=""Test.cs""/></ItemGroup></Project>");
+
+            var solution = new FakeSolution(@"c:\test\fake.sln");
+            solution.Projects.Add(project);
+
+            var request = new AddToProjectRequest
+            {
+                FileName = @"c:\test\code\Test.cs"
+            };
+
+            var handler = new AddToProjectHandler(solution);
+            handler.AddToProject(request);
+
+            Assert.That(project.AsXml(), Is.EqualTo(expectedXml));
+        }
+    }
+}
