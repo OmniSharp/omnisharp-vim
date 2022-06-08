2b39050834433bb02e175e094c1fcbf86f4776e9
Nick Jensen <nj@fluid-solutions.dk>
2022-06-08 23:44:58 +1200

    Respect use_net6 setting on Windows installation

--- installer/omnisharp-manager.ps1
+++ installer/omnisharp-manager.ps1
@@ -5,6 +5,7 @@
 # Options:
 # -v | version to use (otherwise use latest)
 # -l | where to install the server
+# -6 | install the net6.0 server version
 # -u | help / usage info
 # -H | install the HTTP version of the server
 
@@ -12,12 +13,13 @@
 param(
     [Parameter()][Alias('v')][string]$version,
     [Parameter()][Alias('l')][string]$location = "$($Env:USERPROFILE)\.omnisharp\",
+    [Parameter()][Alias('6')][Switch]$use_net6,
     [Parameter()][Alias('u')][Switch]$usage,
     [Parameter()][Alias('H')][Switch]$http_check
 )
 
 if ($usage) {
-    Write-Host "usage:" $MyInvocation.MyCommand.Name "[-Hu] [-v version] [-l location]"
+    Write-Host "usage:" $MyInvocation.MyCommand.Name "[-Hu] [-v version] [-6] [-l location]"
     exit
 }
 
@@ -32,6 +34,12 @@ if ([string]::IsNullOrEmpty($version)) {
     $version = get_latest_version
 }
 
+if ($use_net6) {
+    $net6 = "-net6.0"
+} else {
+    $net6 = ""
+}
+
 if ($http_check) {
     $http = ".http"
 } else {
@@ -44,7 +52,7 @@ if ([Environment]::Is64BitOperatingSystem) {
     $machine = "x86"
 }
 
-$url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/$($version)/omnisharp$($http)-win-$($machine).zip"
+$url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/$($version)/omnisharp$($http)-win-$($machine)$($net6).zip"
 $out = "$($location)\omnisharp$($http)-win-$($machine).zip"
 
 if (Test-Path -Path $location) {
